a17328841b460602e177d6cb2827498c5db15a7c
Atrolide <loszewskigrzegorz@gmail.com>
2024-05-19 13:23:05 +0200

    014: add model validator =

--- src/modules/models/inputs/app_inputs.py
+++ src/modules/models/inputs/app_inputs.py
@@ -1,6 +1,6 @@
 """Module for app inputs"""
 
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
 
 #TODO: #3 Add validators for each class
 class LoginInput(BaseModel):
@@ -14,3 +14,12 @@ class RegisterInput(LoginInput):
     """Input model for user registration"""
 
     confirm_password: str
+
+    @model_validator(mode="after")
+    def check_pass_match(self) -> 'RegisterInput':
+        """Model validator for if passwords match"""
+        confirm_password = self.confirm_password
+        password = self.password
+        if confirm_password != password:
+            raise ValueError("Passwords do not match!")
+        return self
