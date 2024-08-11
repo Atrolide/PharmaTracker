f7315f61910882c8144cea93c181a0ad1a03c56b
Atrolide <loszewskigrzegorz@gmail.com>
2024-08-11 13:54:30 +0200

    049: Create ecs cluster

---
+++ infrastructure/modules/ecs_cluster/main.tf
@@ -0,0 +1,6 @@
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = var.cluster_name
+  tags = {
+    Project = var.tag
+  }
+}
---
+++ infrastructure/modules/ecs_cluster/variables.tf
@@ -0,0 +1,7 @@
+variable "cluster_name" {
+  type        = string
+  description = "Name of the ECS cluster"
+}
+variable "tag" {
+  type = string
+}
